511b66168e33b7d893b43836ffd813bae3648943
ExPLICIT-27 <nandumahesh2004@gmail.com>
2025-04-13 20:04:24 +0530

    Joined everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-
-import './App.css'
-import { Question1 } from '../Components/Question1'
+import "./App.css";
+import { Question1 } from "../Components/Question1";
+import { Question2 } from "../Components/Question2";
+import { ProfileImage } from "../Components/ProfileImage";
+import { UserInfo } from "../Components/UserInfo";
+import { UserPosts } from "../Components/UserPosts";
 function App() {
-
   return (
-    <div className = "">
+    <div className="main">
       <Question1 />
+      <Question2 />
+      <ProfileImage />
+      <UserInfo />
+      <UserPosts />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
