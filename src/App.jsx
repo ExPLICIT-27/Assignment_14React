068ac33b8e83b875fefbe658c5b0edfc6d25b8f1
Sreeram Athrij <sreeramathrij344@gmail.com>
2025-04-13 15:30:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import { Question1 } from '../Components/Question1'
+function App() {
+
+  return (
+    <div className = "">
+      <Question1 />
+    </div>
+  )
+}
+
+export default App
